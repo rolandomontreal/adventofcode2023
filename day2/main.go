47914de1e1469c45d4b676dd2edbb90510073081
rolando <jakob.b.work2@gmail.com>
2023-12-02 14:46:14 +0100

    Solution for pt 2 day 2 working

--- day2/main.go
+++ day2/main.go
@@ -7,12 +7,6 @@ import (
 	"strings"
 )
 
-
-type cubeSet struct {
-	color string
-
-}
-
 func main() {
 	filepath := "./actualdata.txt"
 	bs, err := os.ReadFile(filepath)
@@ -33,15 +27,53 @@ func main() {
 	rows := strings.Split(data, "\n")
 
 	sumOfPossibleRows := 0
+	sumOfMinimumPowers := 0
 	for _, row := range rows {
 		possibleRow, id := possibleSet(row, restriction)
-		fmt.Printf("Row possible: %t, id: %d\n", possibleRow, id)
 		if possibleRow {
 			sumOfPossibleRows += id
 		}
+
+		// Least possible combination
+		lpc := getLeastPossibleCombination(row)
+		power := lpc["red"] * lpc["green"] * lpc["blue"]
+		sumOfMinimumPowers += power
 	}
-	
-	fmt.Println("Result: ", sumOfPossibleRows)
+	fmt.Println("Result: ", sumOfMinimumPowers)
+}
+
+func getLeastPossibleCombination(row string) map[string]int {
+	output := map[string]int {
+		"red": 0,
+		"green": 0,
+		"blue": 0,
+	}
+
+	splitted := strings.Split(row, ":")
+
+	_, joinedSets := splitted[0], splitted[1]
+
+	sets := strings.Split(joinedSets, ";")
+
+	for _, set := range sets {
+		perColor := strings.Split(set, ",")
+		
+		for _, color := range perColor {
+			c := strings.TrimSpace(color)
+			splitted := strings.Split(c, " ")
+			n, err := strconv.Atoi(splitted[0])
+			if err != nil {
+				fmt.Println("Could not count instances in set")
+				os.Exit(1)
+			}
+			color := splitted[1]
+			if output[color] < n {
+				output[color] = n
+			}
+		}
+	}
+
+	return output
 }
 
 func possibleSet(row string, restrictions map[string]int) (bool, int) {
@@ -75,7 +107,7 @@ func possibleSet(row string, restrictions map[string]int) (bool, int) {
 				isPossible = false
 			}
 		}
-	}	
+	}
 
 	return isPossible, gameId
 }
