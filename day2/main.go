ce0de7292aeeded552d750df9d62da9eb2a274e5
rolando <jakob.b.work2@gmail.com>
2023-12-02 13:13:07 +0100

    Solution for pt 1 working

--- day2/main.go
+++ day2/main.go
@@ -14,7 +14,7 @@ type cubeSet struct {
 }
 
 func main() {
-	filepath := "./testdata.txt"
+	filepath := "./actualdata.txt"
 	bs, err := os.ReadFile(filepath)
 
 	if err != nil {
@@ -30,27 +30,30 @@ func main() {
 		"blue": 14,
 	}
 
-	rows := strings.Split(data, "\n")[0:5]
+	rows := strings.Split(data, "\n")
 
+	sumOfPossibleRows := 0
 	for _, row := range rows {
-		fmt.Println("Running logic for row: ", row)
 		possibleRow, id := possibleSet(row, restriction)
 		fmt.Printf("Row possible: %t, id: %d\n", possibleRow, id)
+		if possibleRow {
+			sumOfPossibleRows += id
+		}
 	}
+	
+	fmt.Println("Result: ", sumOfPossibleRows)
 }
 
 func possibleSet(row string, restrictions map[string]int) (bool, int) {
 	splitted := strings.Split(row, ":")
 
 	game, joinedSets := splitted[0], splitted[1]
-	fmt.Println("game: ", game)
 	gameId, err := strconv.Atoi(strings.Split(game, " ")[1])
 	if err != nil {
 		fmt.Println("Could not parse game it: ", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("joinedSets: ", joinedSets)
 	sets := strings.Split(joinedSets, ";")
 
 	isPossible := true
@@ -61,16 +64,13 @@ func possibleSet(row string, restrictions map[string]int) (bool, int) {
 		for _, color := range perColor {
 			c := strings.TrimSpace(color)
 			splitted := strings.Split(c, " ")
-			fmt.Println(splitted)
 			n, err := strconv.Atoi(splitted[0])
 			if err != nil {
 				fmt.Println("Could not count instances in set")
 				os.Exit(1)
 			}
-			fmt.Println(n)
 			color := splitted[1]
 			restriction := restrictions[color]
-			fmt.Println(restriction)
 			if n > restriction {
 				isPossible = false
 			}
