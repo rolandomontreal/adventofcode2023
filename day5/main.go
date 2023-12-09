d3e4f4d28d1e9c9762c562119e96a932021ff904
rolando <jakob.b.work2@gmail.com>
2023-12-09 21:00:34 +0100

    Solution working, feels like worst piece of code I ever written

--- day5/main.go
+++ day5/main.go
@@ -7,16 +7,15 @@ import (
 	"strings"
 )
 
-// seed-to-soil map:
-// Destination range start, source range start, range length
-// 50 98 2
-// 52 50 48
-
-
 type plantingMap struct {
 	name string
 	destinationRangeStart int
 	sourceRangeStart int
+	maprange int
+}
+
+type seedMap struct {
+	startnum int
 	maprange int
 }
 
@@ -31,39 +30,70 @@ func main() {
 
 	data := string(bs)
 	sections := strings.Split(data, "\n\n")
-	seeds := parseSeeds(sections[0])
-	results := [][]int{seeds}
+	// seeds := parseSeedsPt1(sections[0])
+	seeds := parseSeedsPt2(sections[0])
+	fmt.Println(seeds)
+	// results := [][]int{seeds}
 
 	sections = sections[1:]
 	// fmt.Println(sections)
+	parsedSections := parseAllRanges(sections)
+	// fmt.Println(parsedSections)
+	minValue := 9223372036854775807
+	for _, sm := range seeds {
+		fmt.Println("New seed range: ", sm)
+		for i := sm.startnum; i < sm.startnum + sm.maprange; i ++ {
+			endResult := mapNumThroughAll(i, parsedSections)
+			// fmt.Printf("endResult for %d: %d\n\n", i, endResult)
+			if (endResult < minValue) {
+				minValue = endResult
+				fmt.Println("NEW MIN VALUE: ", endResult)
+			}
+		}
+	}
 
+	// Pt 1
 	// Each section is a mapping
-	for _, section := range sections {
-		// Parse to proper format
-		parsedSection := parseRanges(section)
-		// mappedNums := []int{}
-		// Use latest results from mapping from previous step
-		latestResults := results[len(results)-1]
-		newResults := []int{}
-		for _, num := range latestResults {
-			// fmt.Println("Mapping num: ", num)
-			result := mapNum(parsedSection, num)
-			// fmt.Println("Result: ", result, "\n")
-			newResults = append(newResults, result)
-		}
-		results = append(results, newResults)
-		fmt.Println("All results after iteration: ", results)
-	}
+	// for _, section := range sections {
+	// 	// Parse to proper format
+	// 	parsedSection := parseRanges(section)
+	// 	// mappedNums := []int{}
+	// 	// Use latest results from mapping from previous step
+	// 	latestResults := results[len(results)-1]
+	// 	newResults := []int{}
+	// 	for _, num := range latestResults {
+	// 		// fmt.Println("Mapping num: ", num)
+	// 		result := mapNum(parsedSection, num)
+	// 		// fmt.Println("Result: ", result, "\n")
+	// 		newResults = append(newResults, result)
+	// 	}
+	// 	results = append(results, newResults)
+	// 	// fmt.Println("All results after iteration: ", results)
+	// }
 
-	lastResult := results[len(results) - 1]
-	fmt.Println("\n" ,lastResult)
-	smallest := lastResult[0]
-	for _, n := range lastResult {
-		if n < smallest {
-			smallest = n
-		}
+	// lastResult := results[len(results) - 1]
+	// // fmt.Println("\n" ,lastResult)
+	// smallest := lastResult[0]
+	// for _, n := range lastResult {
+	// 	if n < smallest {
+	// 		smallest = n
+	// 	}
+	// }
+	// fmt.Println("Smallest num found: ", smallest)
+}
+
+func mapNumThroughAll(n int, parsedSections [][]plantingMap) int {
+	results := []int{n}
+	for _, parsedSection := range parsedSections {
+		// fmt.Println("Current section: ", parsedSection)
+		// fmt.Println(results)
+		latestResult := results[len(results) - 1]
+		// fmt.Println("latestResult: ", latestResult)
+		result := mapNum(parsedSection, latestResult)
+		// fmt.Printf("Result for %d: %d\n\n", latestResult, result)
+		results = append(results, result)
 	}
-	fmt.Println("Smallest num found: ", smallest)
+	return results[len(results) - 1]
 }
 
 func mapNum(pms []plantingMap, n int) int {
@@ -82,7 +112,30 @@ func mapNum(pms []plantingMap, n int) int {
 	return output
 }
 
-func parseSeeds(seedsRow string) []int {
+func parseSeedsPt2(seedsRow string) []seedMap {
+	output := []seedMap{}
+
+	numsStr := strings.Fields(strings.Split(seedsRow, ":")[1])
+	for i := 0; i < len(numsStr); i += 2 {
+		baseNum, err := strconv.Atoi(numsStr[i])
+		r, err2 := strconv.Atoi(numsStr[i + 1])
+		if err != nil || err2 != nil {
+			fmt.Println("Error parsing seeds for part2: ", err, err2)
+			os.Exit(1)
+		}
+		sm := seedMap{
+			startnum: baseNum,
+			maprange: r,
+		}
+		output = append(output, sm)
+		// fmt.Printf("Seeds generated for %d, with range %d:\n", baseNum, r)
+		// fmt.Println(output)
+	}
+
+	return output
+}
+
+func parseSeedsPt1(seedsRow string) []int {
 	output := []int{}
 
 	numsStr := strings.Fields(strings.Split(seedsRow, ":")[1])
@@ -95,6 +148,15 @@ func parseSeeds(seedsRow string) []int {
 		output = append(output, n)
 	}
 
+	return output
+}
+
+func parseAllRanges(allRanges []string) [][]plantingMap {
+	output := [][]plantingMap{}
+	for _, sm := range allRanges {
+		parsedSection := parseRanges(sm)
+		output = append(output, parsedSection)
+	}
 	return output
 }
 
